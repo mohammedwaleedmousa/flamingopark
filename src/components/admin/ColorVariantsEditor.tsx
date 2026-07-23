3039ce2ef693cfd5fa1d2899d54a5815bf773297
mohammedwaleedmousa <aiengineer77@icloud.com>
2026-07-24 01:32:51 +0300

    update productsformpage

--- src/components/admin/ColorVariantsEditor.tsx
+++ src/components/admin/ColorVariantsEditor.tsx
@@ -150,7 +150,7 @@ if (isHEIC) {
         const compressedFile = await imageCompression(imageFile, {
           maxSizeMB: 1,
           maxWidthOrHeight: 1200,
-          useWebWorker: false,
+          useWebWorker: true,
           fileType: 'image/webp',
         });
 

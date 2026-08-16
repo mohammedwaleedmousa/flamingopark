cdec8b9940a19ae1bc8338584516dda927df9977
Mohammed Waleed <aiengineer77@icloud.com>
2026-08-17 01:46:05 +0300

    Add curated storefront page

---
+++ src/pages/CuratedPage.tsx
@@ -0,0 +1,5 @@
+import CollectionPage from "./CollectionPage";
+
+const CuratedPage = () => <CollectionPage collection="curated" eyebrow="Flamingo Picks" title="مختارات فلامنجو" description="منتجات منتقاة بعناية من فريق فلامنجو." />;
+
+export default CuratedPage;

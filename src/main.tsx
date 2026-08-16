13ffc3bbc7bc867a8c024ccbd4975a366267343f
Mohammed Waleed <aiengineer77@icloud.com>
2026-08-17 01:48:32 +0300

    Load customer desktop page refinements

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./desktop-storefront.css";
+import "./desktop-pages.css";
 
 createRoot(document.getElementById("root")!).render(<App />);

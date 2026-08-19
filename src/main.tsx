c93cb35dd3a72bdb8c3d2e6e92326230fce8fe3d
Mohammed Waleed <aiengineer77@icloud.com>
2026-08-19 20:08:27 +0300

    Load storefront compatibility and layout fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
+import "./lib/cryptoCompat";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import AppErrorBoundary from "./components/AppErrorBoundary";
 import "./index.css";
 import "./desktop-storefront.css";
 import "./desktop-pages.css";
+import "./storefront-layout-fixes.css";
 
 createRoot(document.getElementById("root")!).render(<AppErrorBoundary><App /></AppErrorBoundary>);

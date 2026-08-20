766f1bafc442581808fd1d617b4fc7b0bede5e05
Mohammed Waleed <aiengineer77@icloud.com>
2026-08-20 17:49:23 +0300

    Load phone login compatibility fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import "./lib/cryptoCompat";
+import "./lib/customerAuthCompat";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import AppErrorBoundary from "./components/AppErrorBoundary";

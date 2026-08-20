f10f83efbca601726e4accf73ad4d411afe3acf6
Mohammed Waleed <aiengineer77@icloud.com>
2026-08-20 16:35:57 +0300

    Use real product review summaries

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import "./lib/cryptoCompat";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import AppErrorBoundary from "./components/AppErrorBoundary";
+import ProductRatingSync from "./components/ProductRatingSync";
 import "./index.css";
 import "./desktop-storefront.css";
 import "./desktop-pages.css";
 
-createRoot(document.getElementById("root")!).render(<AppErrorBoundary><App /></AppErrorBoundary>);
+createRoot(document.getElementById("root")!).render(<AppErrorBoundary><><ProductRatingSync /><App /></></AppErrorBoundary>);

d0fdf876fed473312b457b613be7c9651159fb33
Mohammed Waleed <aiengineer77@icloud.com>
2026-09-08 17:34:12 +0300

    Compose desktop home experience

---
+++ src/components/DesktopHomeExperience.tsx
@@ -0,0 +1,4 @@
+import HeroSlider from "@/components/HeroSlider";
+import DesktopHomeTrustBar from "@/components/DesktopHomeTrustBar";
+const DesktopHomeExperience=()=> <><HeroSlider/><DesktopHomeTrustBar/></>;
+export default DesktopHomeExperience;
